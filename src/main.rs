132cd36e176f4e3bcd0cbef35eca25339747f508
Danielle Douglas <ddouglas87@gmail.com>
2019-11-15 15:37:09 -0800

    Now captures Twitter Stream and decodes incoming json data to struct

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,19 @@
 use std::{fs, thread};
 use std::sync::{Arc, Mutex};
 
-use twitter_stream::Token;
+use twitter_stream::{Token, TwitterStreamBuilder};
+use twitter_stream::rt::{self, Future, Stream};
 use yaml_rust::YamlLoader;
+use rustc_serialize::json;
+use chrono::{ParseResult, DateTime, FixedOffset};
 
+/// https://developer.twitter.com/en/docs/tweets/data-dictionary/overview/tweet-object
+struct Tweet {
+    created_at: ParseResult<DateTime<FixedOffset>>,
+    text: String,
+}
+
+#[derive(Debug)]
 struct SentimentCount {
     temp: i32,
 }
@@ -18,15 +28,52 @@ impl SentimentCount {
 
 fn main() {
     let sc = Arc::new(Mutex::new(SentimentCount::new()));
+
+    let sc_clone = sc.clone();
     let backend_handle = thread::spawn(|| {
-        backend(sc)
+        backend(sc_clone)
     });
 
     backend_handle.join().unwrap();
 }
 
+/// Single threaded, due to sentiment analysis not needing a lot of processing power.
 fn backend(sentiment_count: Arc<Mutex<SentimentCount>>) {
     let tokens = load_twitter_tokens("twitter_tokens.yaml");
+    let keywords = "twitter, facebook, google, travel, art, music, photography, love, fashion, food";
+
+    let future = TwitterStreamBuilder::filter(tokens)
+        .track(Some(keywords))
+        .listen()
+        .unwrap()
+        .flatten_stream()
+        .for_each(|json| {
+            let tweet = json_decode(json.to_string());
+            Ok(())
+        })
+        .map_err(|e| eprintln!("error: {}", e));
+
+    rt::run(future);
+}
+
+fn json_decode(json: String) -> Option<Tweet> {
+    #[derive(Debug)]
+    #[derive(RustcDecodable)]
+    struct JSONTweet { created_at: String, text: String, lang: Option<String> }
+    let decoded: json::DecodeResult<JSONTweet> = json::decode(&json);
+
+    if let Ok(t) = decoded {
+        if let Some(lang) = &t.lang {
+            if lang.starts_with("en") {
+                return Some(Tweet {
+                    created_at: DateTime::parse_from_str(&t.created_at, "%a %b %d %H:%M:%S %z %Y"),
+                    text: t.text,
+                })
+            }
+        }
+    }
+
+    None
 }
 
 fn load_twitter_tokens(filename: &str) -> Token {
